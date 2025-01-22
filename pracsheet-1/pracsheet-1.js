b41e8dcf85e20dd81ef00769295f0b909b33bf6c
suba <nahian.suba84@gmail.com>
2025-01-23 02:14:57 +0600

    javascript

---
+++ pracsheet-1/pracsheet-1.js
@@ -0,0 +1,109 @@
+//prob-1
+/*var num = 5;
+if (num % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+*/
+
+//prob-2
+/*var num1 = 6;
+var num2 = 4;
+var num3 = 8;
+if (num1 > num2 && num1 > num3) {
+  console.log("Number 1 is Largest");
+} else if (num2 > num3) {
+  console.log("Number 2 is largest");
+} else {
+  console.log("Number 3 is largest");
+}
+*/
+
+//prob-3
+/*var mark = 95;
+if (mark >= 80) {
+  console.log("Grade: A");
+} else if (marks >= 60) {
+  console.log("Grade: B");
+} else if (marks >= 40) {
+  console.log("Grade: C");
+} else {
+  console.log("Grade: F");
+}*/
+
+//prob- 4
+/*var year = 2025;
+if (year % 400 == 0) {
+  console.log("leap year");
+} else if (year % 100 == 0) {
+  console.log("not leap year");
+} else if (year % 4 == 0) {
+  console.log("leap year");
+} else {
+  console.log("not leap year");
+}
+*/
+
+//prob-5
+/*var num = 15;
+if (num % 2 === 0) {
+  if (num % 5 === 0) {
+    console.log("Even and divisible by 5");
+  }
+} else {
+  console.log("Not Both");
+}
+*/
+
+//prob-6
+/*var mark = 99;
+if (mark >= 33) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
+*/
+
+//prob-7
+/*var month=
+if (month === "february") {
+    console.log("28 or 29 days");
+} else if (month === "april" || month === "june" || month === "september" || month === "november") {
+    console.log("30 days");
+} else if (month === "january" || month === "march" || month === "may" || month === "july" || month === "august" || month === "october" || month === "december") {
+    console.log("31 days");
+} else {
+    console.log("Invalid month name");
+}
+*/
+//prob-8
+/*var age = 15;
+if (age >= 18) {
+  console.log("Eligible for driving license");
+} else {
+  console.log("Not eligible");
+}
+*/
+
+//prob-9
+/*var num = 2;
+if (num > 0) {
+  console.log("Positive");
+} else if (num < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+*/
+
+//prob-10
+/*var day = "Monday";
+if (day === "Saturday") {
+  console.log("Holiday");
+} else if (day === "Monday") {
+  console.log("Holiday");
+} else {
+  console.log("Working day");
+}
+*/
