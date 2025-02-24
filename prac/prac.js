1730eb27021f373a5cfb1739b581ca04c68f4f62
suba <nahian.suba84@gmail.com>
2025-02-25 01:01:09 +0600

    prac

---
+++ prac/prac.js
@@ -0,0 +1,24 @@
+//task--1
+
+let a = 10;
+console.log(a);
+
+var b = 20;
+console.log(b);
+
+const c = 30;
+console.log(c);
+
+//task-2
+
+let x = 10;
+let y = 30;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+//task-3
+
+console.log("life" + " para");
+console.log("life" + " =bash");
