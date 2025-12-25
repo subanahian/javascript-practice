81cb2db766439e90c9afc0742af60b7823f6c2a3
suba <nahian.suba84@gmail.com>
2025-12-25 22:44:08 +0600

    p2

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -524,4 +524,17 @@ function findMissingNumberSet(nums) {
     }
     return -1;
 }
+//prob-67
+function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
 
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
