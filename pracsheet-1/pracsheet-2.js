1deb2bc03c104e1fdc23ee8dbb9f6cdadbe83329
suba <nahian.suba84@gmail.com>
2025-12-28 00:01:26 +0600

    jss

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -575,3 +575,19 @@ function threeSum(nums) {
 }
 
 console.log(threeSum([-1,0,1,2,-1,-4])); // [[-1,-1,2],[-1,0,1]]
+//prob-70
+function subarraySum(nums, k) {
+    let count = 0, sum = 0;
+    const map = new Map([[0, 1]]);
+    
+    for (const num of nums) {
+        sum += num;
+        if (map.has(sum - k)) {
+            count += map.get(sum - k);
+        }
+        map.set(sum, (map.get(sum) || 0) + 1);
+    }
+    return count;
+}
+
+console.log(subarraySum([1, 1, 1], 2)); // 2
