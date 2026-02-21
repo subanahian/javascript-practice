e6a27d6cd2a302bd3df54da3892fa6c0b524bf72
suba <nahian.suba84@gmail.com>
2026-02-22 02:11:22 +0600

    js

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -896,3 +896,13 @@ function binarySearch(arr,target){
   }
   return -1;
 }
+//prob-121
+function hasCycle(head){
+  let slow=head, fast=head;
+  while(fast && fast.next){
+    slow=slow.next;
+    fast=fast.next.next;
+    if(slow===fast) return true;
+  }
+  return false;
+}
