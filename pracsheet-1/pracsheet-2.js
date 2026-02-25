21f1cad81a2f33d50a095885ed239b514c8f5d3f
suba <nahian.suba84@gmail.com>
2026-02-26 00:04:44 +0600

    js

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -932,3 +932,13 @@ document.getElementById("list").addEventListener("click", function(e){
     console.log(e.target.innerText);
   }
 });
+//prob-125
+function deepFreeze(obj){
+  Object.freeze(obj);
+  Object.keys(obj).forEach(key => {
+    if(typeof obj[key] === "object" && obj[key] !== null){
+      deepFreeze(obj[key]);
+    }
+  });
+  return obj;
+}
