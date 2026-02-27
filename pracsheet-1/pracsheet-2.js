15b89c664c23e401bed92541e9773cb83b4debaa
suba <nahian.suba84@gmail.com>
2026-02-28 01:32:51 +0600

    js

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -953,4 +953,13 @@ function myInterval(fn, delay){
   }
   run();
   return id;
+}//prob-127
+function hasCycle(head){
+  let slow=head, fast=head;
+  while(fast && fast.next){
+    slow=slow.next;
+    fast=fast.next.next;
+    if(slow===fast) return true;
+  }
+  return false;
 }
