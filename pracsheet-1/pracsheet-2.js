a1c5eda256c9602ff0a47327dc12a1f93e9d67e0
suba <nahian.suba84@gmail.com>
2026-03-01 00:21:26 +0600

    k

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -963,3 +963,12 @@ function hasCycle(head){
   }
   return false;
 }
+//prob-128
+function twoSum(arr,target){
+  let map = {};
+  for(let i=0;i<arr.length;i++){
+    let diff = target - arr[i];
+    if(map[diff] !== undefined) return [map[diff], i];
+    map[arr[i]] = i;
+  }
+}
