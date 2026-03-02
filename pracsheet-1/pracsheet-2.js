fb4b5d97abd8aeaad6b4ae261a91607f454ffef7
suba <nahian.suba84@gmail.com>
2026-03-02 22:46:10 +0600

    js

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -972,3 +972,13 @@ function twoSum(arr,target){
     map[arr[i]] = i;
   }
 }
+//prob-129
+class Stack {
+  constructor(){
+    this.items = [];
+  }
+  push(val){ this.items.push(val); }
+  pop(){ return this.items.pop(); }
+  peek(){ return this.items[this.items.length-1]; }
+  isEmpty(){ return this.items.length === 0; }
+}
