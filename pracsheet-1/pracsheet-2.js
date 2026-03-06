0801cec86a09771d459612db2d4d42cff054ed38
suba <nahian.suba84@gmail.com>
2026-03-06 23:28:53 +0600

    j

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -995,3 +995,12 @@ let n = 10;
 let sum = 0;
 for (let i = 1; i <= n; i++) sum += i;
 console.log(sum);
+//prob-132
+function maxSubArray(arr){
+  let max=arr[0], curr=arr[0];
+  for(let i=1;i<arr.length;i++){
+    curr=Math.max(arr[i], curr+arr[i]);
+    max=Math.max(max,curr);
+  }
+  return max;
+}
