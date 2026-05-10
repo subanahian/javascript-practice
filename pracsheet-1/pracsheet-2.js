eb67a14709ce5493d09b69aaf474d6c38d0d9dae
suba <nahian.suba84@gmail.com>
2026-05-10 20:58:31 +0600

    l

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -1543,3 +1543,19 @@ function dailyTemp(T){
 
   return res;
 }
+//prob-224
+function twoSum(nums, target) {
+    let map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (map.hasOwnProperty(complement)) {
+            return [map[complement], i];
+        }
+
+        map[nums[i]] = i;
+    }
+}
+
+console.log(twoSum([2,7,11,15], 9));
