503f73bf8c4f17c22af32bd1ffe833d25ae859d2
suba <nahian.suba84@gmail.com>
2026-05-22 00:05:44 +0600

    f

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -1705,3 +1705,16 @@ function capitalize(str) {
 }
 
 console.log(capitalize("hello world"));
+
+//prob-236
+function factorial(n) {
+  let result = 1;
+
+  for(let i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+console.log(factorial(5));
