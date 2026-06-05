08d3ef42b82f5f87ca38987c6439778223ed9ceb
suba <nahian.suba84@gmail.com>
2026-06-05 11:09:41 +0600

    l'

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -1866,3 +1866,14 @@ function shortestWord(str) {
 console.log(shortestWord("I love javascript"));
 
 //prob-250
+function twoSum(arr, target) {
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[i] + arr[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum([2,7,11,15], 9));
