b4a307fa5d9bfcf4d827733a863a9f630cb82a2e
suba <nahian.suba84@gmail.com>
2026-06-10 00:00:15 +0600

    g

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -1907,4 +1907,8 @@ function rotateLeft(arr) {
 }
 
 console.log(rotateLeft([1,2,3,4]));
+
 //prob-254
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.map(num => num * 2);
+console.log(result);
