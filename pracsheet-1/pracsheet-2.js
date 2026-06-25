59d92b67c140b94716409997dfeb4addfa764c0b
suba <nahian.suba84@gmail.com>
2026-06-25 19:52:39 +0600

    k

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -2105,4 +2105,28 @@ function firstUniqueChar(str) {
     return null;
 }
 
+//prob-266
+function longestConsecutive(nums) {
+    const set = new Set(nums);
+    let longest = 0;
+
+    for (const num of set) {
+        if (!set.has(num - 1)) {
+            let current = num;
+            let length = 1;
+
+            while (set.has(current + 1)) {
+                current++;
+                length++;
+            }
+
+            longest = Math.max(longest, length);
+        }
+    }
+
+    return longest;
+}
+
+console.log(longestConsecutive([100,4,200,1,3,2]));
+
 console.log(firstUniqueChar("aabbcdeeff"));
