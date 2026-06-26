f9cad3570a2f11334d6f229053412750504632ba
suba <nahian.suba84@gmail.com>
2026-06-26 19:10:14 +0600

    prob-267

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -2130,3 +2130,29 @@ function longestConsecutive(nums) {
 console.log(longestConsecutive([100,4,200,1,3,2]));
 
 console.log(firstUniqueChar("aabbcdeeff"));
+
+//prob-267
+function productExceptSelf(nums){
+
+    const result=new Array(nums.length).fill(1);
+
+    let left=1;
+
+    for(let i=0;i<nums.length;i++){
+
+        result[i]=left;
+
+        left*=nums[i];
+    }
+
+    let right=1;
+
+    for(let i=nums.length-1;i>=0;i--){
+
+        result[i]*=right;
+
+        right*=nums[i];
+    }
+
+    return result;
+}
