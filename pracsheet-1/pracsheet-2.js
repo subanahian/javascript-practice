e4c30b5cd78b3319edd9e305df672eb6aff206e5
suba <nahian.suba84@gmail.com>
2026-06-27 12:56:46 +0600

    orib-268

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -2156,3 +2156,20 @@ function productExceptSelf(nums){
 
     return result;
 }
+
+//prob-268
+function maxSubArray(nums){
+
+    let max=nums[0];
+
+    let current=nums[0];
+
+    for(let i=1;i<nums.length;i++){
+
+        current=Math.max(nums[i],current+nums[i]);
+
+        max=Math.max(max,current);
+    }
+
+    return max;
+}
