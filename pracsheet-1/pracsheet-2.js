813f241bb98262ac15a0b7c190ae8bd83dccfe9d
suba <nahian.suba84@gmail.com>
2026-07-12 00:30:26 +0600

    k

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -2399,3 +2399,22 @@ function permute(str) {
 
     return result;
 }
+//prob-279
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) return mid;
+
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
