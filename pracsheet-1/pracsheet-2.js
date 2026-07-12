34aa3d4a456b6294e7e079ba944b683ec083ae32
suba <nahian.suba84@gmail.com>
2026-07-13 00:01:42 +0600

    d

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -2436,3 +2436,22 @@ function permute(str) {
 
     return result;
 }
+
+//prob-281
+function flattenObject(obj, prefix = "", result = {}) {
+    for (const key in obj) {
+        const newKey = prefix ? `${prefix}.${key}` : key;
+
+        if (
+            typeof obj[key] === "object" &&
+            obj[key] !== null &&
+            !Array.isArray(obj[key])
+        ) {
+            flattenObject(obj[key], newKey, result);
+        } else {
+            result[newKey] = obj[key];
+        }
+    }
+
+    return result;
+}
