42eddf99da49c3af200481acd4bf944db2569e53
suba <nahian.suba84@gmail.com>
2026-09-03 22:41:07 +0600

    k

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -3161,3 +3161,36 @@ function findDuplicateUsers(users) {
       arr.findIndex(u => u.id === user.id) === index
   );
 }
+
+//prob-321
+function deepEqual(a, b) {
+  if (a === b) return true;
+
+  if (
+    typeof a !== "object" ||
+    typeof b !== "object" ||
+    a === null ||
+    b === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (const key of keysA) {
+    if (!Object.hasOwn(b, key)) {
+      return false;
+    }
+
+    if (!deepEqual(a[key], b[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
