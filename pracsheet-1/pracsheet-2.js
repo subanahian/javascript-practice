13cc17932f2430abde7823564ce01fd31afcb254
suba <nahian.suba84@gmail.com>
2026-09-09 22:51:31 +0600

    k

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -3286,3 +3286,19 @@ function myPromiseAllSettled(promises) {
     });
   });
 }
+
+//prob-326
+function counter() {
+  let count = 0;
+
+  return function () {
+    return ++count;
+  };
+}
+
+const a = counter();
+const b = counter();
+
+console.log(a());
+console.log(a());
+console.log(b());
