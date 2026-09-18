f6f7b5290bac7175bb8fc8640555896c6343664e
suba <nahian.suba84@gmail.com>
2026-09-18 19:08:01 +0600

    k

--- pracsheet-1/pracsheet-2.js
+++ pracsheet-1/pracsheet-2.js
@@ -3446,3 +3446,38 @@ function longestPalindrome(s) {
 
   return s.slice(start, end + 1);
 }
+
+//prob-331
+function longestPalindrome(s) {
+  if (s.length < 2) return s;
+
+  let start = 0;
+  let end = 0;
+
+  function expand(left, right) {
+    while (
+      left >= 0 &&
+      right < s.length &&
+      s[left] === s[right]
+    ) {
+      left--;
+      right++;
+    }
+
+    return right - left - 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    const len1 = expand(i, i);
+    const len2 = expand(i, i + 1);
+
+    const len = Math.max(len1, len2);
+
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
+    }
+  }
+
+  return s.slice(start, end + 1);
+}
